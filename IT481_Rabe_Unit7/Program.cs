7a467a9d11f77a5ce7e2fe428672e6cb8e406aa8
Misty Rabe <mistyrabe@student.kaplan.edu>
2018-09-25 16:16:22 -0500

    Add project files.

---
+++ IT481_Rabe_Unit7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+You will select an internal sorting algorithm like a bubble sort or a merge sort and implement it. Sorting algorithms are very well established and many can be found in the Library, your textbooks or by searching the Web. Select one to use for this assignment and make sure you credit the original author and list the URL in your comments (if applicable).
+Create three data sets that you can use with the sort you selected: small, medium and large. An example might be 10 data items for small, 1000 data items for medium and 10,000 data items for large. You may need to adjust the size up or down.
+You can search the Web for “large data” and find a source.
+This just needs to be some text or numbers that you will use with your sort routines.
+After you implement and test the sorting algorithm that you picked, execute it with the three data sets to create an execution baseline. This can be done by marking the beginning execution time and the end time; from those, derive an elapsed time. Make sure you save your results.
+Create a copy of the original sort. This is the version you will modify in order to try to optimize the algorithm.
+Try to improve the algorithm so that the elapsed time improves. Try more than one improvement. Make sure you save results of each run, so you can see if your changes made an improvement.
+An acceptable alternative would be to use a different algorithm from the Web.
+For example, you could test with a bubble sort and a quick sort.
+After you complete this Assignment, remember to save the source code in the source code management system that you selected in Unit 1.
+When you have completed the work, write a 1 page paper (plus a cover page) summarizing your results. Write in full sentences and paragraph form, with subheadings where you feel it is necessary to show the organization of your ideas and content. On another page in the same document, provide a screenshot of your project within your source code management system.
+Review the rubric to ensure that all requirements are met.
+Place your completed code and your summary in a zip file and submit to the Dropbox.*/
+
+namespace IT481_Rabe_Unit7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
